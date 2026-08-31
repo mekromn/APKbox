aa48679e6c29aece63ea4daf027a294782c58b0b
mekromn <67240529+mekromn@users.noreply.github.com>
2026-08-31 14:23:16 -0700

    Bump APKbox to 0.4.0 organization release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.mekromn.apkbox"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.3.2"
+        versionCode = 6
+        versionName = "0.4.0"
     }
 
     signingConfigs {
