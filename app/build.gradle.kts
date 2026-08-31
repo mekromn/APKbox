aa8f39c25204694b6f9bfdad4793e21c77dcfbfa
mekromn <67240529+mekromn@users.noreply.github.com>
2026-08-31 13:25:39 -0700

    Bump APKbox to 0.3.2 APK cleanup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.mekromn.apkbox"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "0.3.1"
+        versionCode = 5
+        versionName = "0.3.2"
     }
 
     signingConfigs {
