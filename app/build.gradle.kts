3f0bec2c1bb524ba5177b2137676ed1e1e8b4e04
mekromn <67240529+mekromn@users.noreply.github.com>
2026-08-31 16:24:22 -0700

    Bump APKbox to 0.6.1 cleanup hotfix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.mekromn.apkbox"
         minSdk = 26
         targetSdk = 36
-        versionCode = 8
-        versionName = "0.6.0"
+        versionCode = 9
+        versionName = "0.6.1"
     }
 
     signingConfigs {
