e5b7314cecf7de35b0c6c118f3278666f33a8035
mekromn <67240529+mekromn@users.noreply.github.com>
2026-08-31 21:35:05 -0700

    Bump APKbox to 0.8.0 Speed Engine

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.mekromn.apkbox"
         minSdk = 26
         targetSdk = 36
-        versionCode = 12
-        versionName = "0.7.1"
+        versionCode = 13
+        versionName = "0.8.0"
     }
 
     signingConfigs {
