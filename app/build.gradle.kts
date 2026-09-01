8f1f8b13ad126014cd70eab0c8a3172e93deb983
mekromn <67240529+mekromn@users.noreply.github.com>
2026-08-31 20:15:13 -0700

    Bump APKbox to 0.7.1 chooser handler hotfix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.mekromn.apkbox"
         minSdk = 26
         targetSdk = 36
-        versionCode = 11
-        versionName = "0.7.0"
+        versionCode = 12
+        versionName = "0.7.1"
     }
 
     signingConfigs {
