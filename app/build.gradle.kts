ed4712159106010623ab8bb6baed062c3149a631
mekromn <67240529+mekromn@users.noreply.github.com>
2026-09-02 12:56:19 -0700

    Bump APKbox to 0.9.0 Remote Debug Bridge

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.mekromn.apkbox"
         minSdk = 26
         targetSdk = 36
-        versionCode = 13
-        versionName = "0.8.0"
+        versionCode = 14
+        versionName = "0.9.0"
     }
 
     signingConfigs {
